d79f10953fbcb0249c540b6f91fdd0936ec8d3a6
feruzJavaDev <feruzgoogacc@gmail.com>
2025-03-20 00:02:48 +0500

    start

--- src/main/java/com/example/cicd/DataLoader.java
+++ src/main/java/com/example/cicd/DataLoader.java
@@ -14,7 +14,7 @@ public class DataLoader implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         if (userRepository.findAll().isEmpty()) {
             User user = new User();
             user.setUsername("admin");
